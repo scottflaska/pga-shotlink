3dfe06f84cbfc123e113e9eb0814e14d88646c14
Scott Flaska <scott.flaska@gmail.com>
2022-06-09 12:59:13 -0600

    update plots

--- utils/result_cut_colors.R
+++ utils/result_cut_colors.R
@@ -1,5 +1,5 @@
 result_cut_colors = c('bunker'='tan',
-                      'fairway'='green',
-                      'green'='lightgreen',
-                      'rough'='darkgreen',
+                      'fairway'='#4CBB17',
+                      'green'='#90EE90',
+                      'rough'='#355E3B',
                       'other'='red')
